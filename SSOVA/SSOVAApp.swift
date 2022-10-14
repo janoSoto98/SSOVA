20d479d0e5c75cb23ec8fa54335a850737901ddb
Alejandro  Soto <alejandrosoto@solemti.mx>
2022-10-14 10:43:54 -0600

    Initial Commit

---
+++ SSOVA/SSOVAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSOVAApp.swift
+//  SSOVA
+//
+//  Created by GrupoSti on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SSOVAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
